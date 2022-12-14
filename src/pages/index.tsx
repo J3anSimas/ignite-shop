c89904e461b2aa51922495fed6225792db0bbc14
Jean Simas <jeansimas16@gmail.com>
2022-12-14 17:21:11 -0300

    Building Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
-import { styled } from '../styles'
-
+import Image from 'next/image'
+import camiseta1 from '../assets/shirts/1.png'
+import camiseta2 from '../assets/shirts/1.png'
+import camiseta3 from '../assets/shirts/1.png'
+import ProductCard from '../components/pages/home/product-card'
 export default function Home() {
-  return <div>Index</div>
+  return (
+    <main className="flex gap-12 w-full min-height-[656px] max-w-[calc(100vw-((100vw_-_1180px)/2))] ml-auto">
+      <ProductCard imageSource={camiseta1} />
+      <ProductCard imageSource={camiseta1} />
+      <ProductCard imageSource={camiseta1} />
+    </main>
+  )
 }
