7851c4dd8cc4e3340ba1a6021460eb40614c7c57
Jean Simas <jeansimas16@gmail.com>
2022-12-14 17:56:56 -0300

    Implementing carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,34 @@ import camiseta1 from '../assets/shirts/1.png'
 import camiseta2 from '../assets/shirts/1.png'
 import camiseta3 from '../assets/shirts/1.png'
 import ProductCard from '../components/pages/home/product-card'
+import { useKeenSlider } from 'keen-slider/react'
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
   return (
-    <main className="flex gap-12 w-full min-height-[656px] max-w-[calc(100vw-((100vw_-_1180px)/2))] ml-auto">
-      <ProductCard imageSource={camiseta1} />
-      <ProductCard imageSource={camiseta1} />
-      <ProductCard imageSource={camiseta1} />
+    <main
+      className="flex w-full min-height-[656px] max-w-[calc(100vw-((100vw_-_1180px)/2))] overflow-hidden ml-auto keen-slider"
+      ref={sliderRef}
+    >
+      <span className="keen-slider__slide ">
+        <ProductCard imageSource={camiseta1} />
+      </span>
+      <span className="keen-slider__slide">
+        <ProductCard imageSource={camiseta1} />
+      </span>
+      <span className="keen-slider__slide">
+        <ProductCard imageSource={camiseta1} />
+      </span>
+      <span className="keen-slider__slide">
+        <ProductCard imageSource={camiseta1} />
+      </span>
+      <span className="keen-slider__slide">
+        <ProductCard imageSource={camiseta1} />
+      </span>
     </main>
   )
 }
