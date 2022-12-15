97de2655a38f111d1dad8c3965c2d2444fc1f066
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-12-14 22:55:13 -0300

    Getting data from props

--- src/components/pages/home/product-card.tsx
+++ src/components/pages/home/product-card.tsx
@@ -1,27 +1,36 @@
 import Image, { StaticImageData } from 'next/image'
 
+import { priceFormatter } from '../../../utils/formatter'
 type TProductCardProps = {
-  imageSource: StaticImageData
+  id: string
+  name: string
+  imageUrl: string
+  price: number
 }
 
 export default function ProductCard({
-  imageSource
+  id,
+  name,
+  imageUrl,
+  price
 }: TProductCardProps): JSX.Element {
   return (
     <a
-      className="bg-gradient-to-b from-[#1ea483] to-[#7465d4] rounded-lg p-1 relative flex flex-col items-center justify-center overflow-hidden group w-full"
-      href=""
+      className="bg-gradient-to-b from-[#1ea483] to-[#7465d4] h-full rounded-lg p-1 relative flex flex-col items-center justify-center overflow-hidden group w-full"
+      href={`/products/${id}`}
     >
       <Image
         className="object-cover"
-        src={imageSource}
-        alt="Camiseta"
+        src={imageUrl}
+        alt={name}
         width={520}
         height={480}
       />
       <footer className="absolute bottom-1 left-1 right-1 rounded-md p-4 flex items-center justify-between bg-gray-800 bg-opacity-90 translate-y-[110%] opacity-0 transition-all group-hover:opacity-100 group-hover:translate-y-0">
-        <strong className="text-lg text-gray-100">Camiseta X</strong>
-        <span className="text-xl font-bold text-green-300">R$ 79,90</span>
+        <strong className="text-lg text-gray-100">{name}</strong>
+        <span className="text-xl font-bold text-green-300">
+          {priceFormatter.format(price)}
+        </span>
       </footer>
     </a>
   )
