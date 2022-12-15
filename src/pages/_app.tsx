29133c99f010b2d05dffce18739a521a7056182e
Jean Simas <jeansimas16@gmail.com>
2022-12-15 17:10:24 -0300

    Adding Link tag to header logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import type { AppProps } from 'next/app'
 import '../styles/global.css'
 import logoImg from '../assets/logo.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex bg-red-500 flex-col items-start min-h-screen justify-center">
       <header className="py-8 w-full max-w-[1180px] my-0 mx-auto">
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <Image src={logoImg} alt="" />
+        </Link>
       </header>
       <Component {...pageProps} />
     </div>
